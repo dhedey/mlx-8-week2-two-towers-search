e0d0b4363ac7a850574e68f6241b418a9d526f5e
David Edey <github@david-edey.com>
2025-06-17 10:59:47 +0100

    Model uses pretrained weights

--- model/train.py
+++ model/train.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
@@ -6,14 +6,66 @@ import torch.nn as nn
 import torch.optim as optim
 import torch
 import random
+import re
+import os
+
+@dataclass
+class TrainingHyperparameters:
+    batch_size: int
+    epochs: int
+    learning_rate: float
+    freeze_embeddings: bool
+    dropout: float
+
+    @classmethod
+    def for_prediction(cls):
+        return cls(
+            batch_size=1,
+            epochs=0,
+            learning_rate=0,
+            freeze_embeddings=True,
+            dropout=0,
+        )
+
+    def to_dict(self):
+        return vars(self)
+
+@dataclass
+class ModelHyperparameters:
+    comparison_embedding_size: int = 128
+    query_tower_hidden_dimensions: list[int] = field(default_factory=lambda: [128, 64])
+    doc_tower_hidden_dimensions: list[int] = field(default_factory=lambda: [128, 64])
+    include_layer_norms: bool = True
+
+    def to_dict(self):
+        return vars(self)
+
+@dataclass
+class ModelTokenizer:
+    token_map: dict[str, int]
+    default_token_embeddings: torch.Tensor
+
+    @classmethod
+    def load(cls):
+        folder = os.path.dirname(__file__)
+        word_vectors = torch.load(folder + '/word_vectors.pt')
+        return cls(
+            token_map={word: i for i, word in enumerate(word_vectors["vocabulary"])},
+            default_token_embeddings=word_vectors["embeddings"],
+        )
     
-VOCAB_SIZE = 60000  # Example vocabulary size, adjust as needed
+    def vocabulary_size(self):
+        return len(self.token_map)
+    
+    def embedding_size(self):
+        return self.default_token_embeddings.shape[1]
+    
+    def tokenize(self, string):
+        filtered_title_words = re.sub(r'[^a-z0-9 ]', '', string.lower()).split()
+        mapped_words = [self.token_map[word] for word in filtered_title_words if word in self.token_map]
+        return mapped_words
 
-def tokenize(string):
-    token_count = random.randint(1, 20)
-    return [random.randint(0, VOCAB_SIZE - 1) for _ in range(token_count)]
-
-def prepare_test_batch(raw_batch, full_dataset, device):
+def prepare_test_batch(tokenizer: ModelTokenizer, raw_batch, full_dataset, device):
     queries = []
     good_documents = []
     bad_documents = []
@@ -44,13 +96,13 @@ def prepare_test_batch(raw_batch, full_dataset, device):
             bad_documents.append(random_passage)
     
     return {
-        "queries": prepare_tokens_for_embedding_bag(queries, tokenize, device),
-        "good_documents": prepare_tokens_for_embedding_bag(good_documents, tokenize, device),
-        "bad_documents": prepare_tokens_for_embedding_bag(bad_documents, tokenize, device),
+        "queries": prepare_tokens_for_embedding_bag(queries, tokenizer, device),
+        "good_documents": prepare_tokens_for_embedding_bag(good_documents, tokenizer, device),
+        "bad_documents": prepare_tokens_for_embedding_bag(bad_documents, tokenizer, device),
         "document_count_for_each_query": document_count_for_each_query, # List not tensor
     }
 
-def prepare_tokens_for_embedding_bag(strings, tokenize_method, device):
+def prepare_tokens_for_embedding_bag(strings, tokenizer: ModelTokenizer, device):
     """
     See https://docs.pytorch.org/docs/stable/generated/torch.nn.EmbeddingBag.html
     """
@@ -60,7 +112,7 @@ def prepare_tokens_for_embedding_bag(strings, tokenize_method, device):
 
     for string in strings:
         offsets.append(current_offset)
-        tokens = tokenize_method(string)
+        tokens = tokenizer.tokenize(string)
         flattened_tokens.extend(tokens)
         current_offset += len(tokens)
 
@@ -79,8 +131,8 @@ def calculate_triplet_loss(query_vectors, good_document_vectors, bad_document_ve
     diff = good_similarity - bad_similarity
     return torch.max(torch.tensor(0), torch.tensor(margin) - diff).sum(dim=0)
 
-def process_test_batch(batch, full_dataset, query_tower, doc_tower, device, margin):
-    prepared = prepare_test_batch(batch, full_dataset, device)
+def process_test_batch(tokenizer, batch, full_dataset, query_tower, doc_tower, device, margin):
+    prepared = prepare_test_batch(tokenizer, batch, full_dataset, device)
 
     query_vectors = query_tower(prepared["queries"])              # tensor of shape (Q, E)
     good_document_vectors = doc_tower(prepared["good_documents"]) # tensor of shape (D, E)
@@ -105,28 +157,95 @@ def process_test_batch(batch, full_dataset, query_tower, doc_tower, device, marg
         "document_count": good_document_vectors.shape[0],
     }
 
+
+@dataclass
+class DocumentTowerParameters:
+    training: TrainingHyperparameters
+    tokenizer: ModelTokenizer
+    model: ModelHyperparameters
+
 class DocumentTower(nn.Module):
-    def __init__(self):
+    def __init__(self, parameters: DocumentTowerParameters):
         super(DocumentTower, self).__init__()
-        self.average_pooling = nn.EmbeddingBag(VOCAB_SIZE, 128, mode='mean')
+        self.model = PooledTowerModel(
+            default_token_embeddings=parameters.tokenizer.default_token_embeddings,
+            training_parameters=parameters.training,
+            hidden_layer_sizes=parameters.model.doc_tower_hidden_dimensions,
+            output_size=parameters.model.comparison_embedding_size,
+        )
 
-    def forward(self, documents):
-        flattened_tokens = documents["flattened_tokens"]
-        offsets = documents["offsets"]
+    def forward(self, queries):
+        return self.model(queries)
 
-        return self.average_pooling(flattened_tokens, offsets)
-    
+
+@dataclass
+class QueryTowerParameters:
+    training: TrainingHyperparameters
+    tokenizer: ModelTokenizer
+    model: ModelHyperparameters
 
 class QueryTower(nn.Module):
-    def __init__(self):
+    def __init__(self, parameters: QueryTowerParameters):
         super(QueryTower, self).__init__()
-        self.average_pooling = nn.EmbeddingBag(VOCAB_SIZE, 128, mode='mean')
+        self.model = PooledTowerModel(
+            default_token_embeddings=parameters.tokenizer.default_token_embeddings,
+            training_parameters=parameters.training,
+            hidden_layer_sizes=parameters.model.query_tower_hidden_dimensions,
+            output_size=parameters.model.comparison_embedding_size,
+        )
+
+    def forward(self, queries):
+        return self.model(queries)
+    
+class PooledTowerModel(nn.Module):
+    def __init__(
+            self,
+            default_token_embeddings: torch.Tensor,
+            training_parameters: TrainingHyperparameters,
+            hidden_layer_sizes: list[int],
+            output_size: int,
+        ):
+        super(PooledTowerModel, self).__init__()
+        self.average_pooling = nn.EmbeddingBag.from_pretrained(
+            embeddings=default_token_embeddings,
+            freeze=training_parameters.freeze_embeddings,
+            mode='mean',
+        )
+        dimension_sizes = hidden_layer_sizes
+
+        input_sizes = [default_token_embeddings.shape[1]] + dimension_sizes
+        output_sizes = dimension_sizes + [output_size]
+        hidden_layer_sizes = zip(input_sizes[:-1], output_sizes[:-1])
+        self.hidden_layers = nn.Sequential(*[
+            HiddenLayer(input_size, output_size, training_parameters.dropout) for input_size, output_size in hidden_layer_sizes
+        ])
+
+        self.output_layer = nn.Linear(input_sizes[-1], output_sizes[-1])
+
 
     def forward(self, queries):
         flattened_tokens = queries["flattened_tokens"]
         offsets = queries["offsets"]
 
-        return self.average_pooling(flattened_tokens, offsets)
+        x = self.average_pooling(flattened_tokens, offsets)
+        x = self.hidden_layers(x)
+        x = self.output_layer(x)
+
+        return x
+
+class HiddenLayer(nn.Module):
+    def __init__(self, input_size, output_size, dropout=0.0):
+        super(HiddenLayer, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(output_size)
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.layer_norm(x)
+        return x
 
 if __name__ == "__main__":
     print("Loading dataset...")
@@ -139,13 +258,39 @@ if __name__ == "__main__":
         train_dataset,
         batch_size=2,
         shuffle=True,
-        collate_fn=lambda x: x,  # No special collate function needed
+        collate_fn=lambda x: x,  # Specify identity collate function (no magic batching which breaks)
     )
 
-    doc_tower = DocumentTower()
-    query_tower = QueryTower()
-    combined_parameters = list(doc_tower.parameters()) + list(query_tower.parameters())
+    tokenizer = ModelTokenizer.load()
+    token_embeddings = tokenizer.default_token_embeddings
+    print(f"Tokenizer loaded. Vocabulary size {token_embeddings.shape[0]}, Embedding size: {token_embeddings.shape[1]}")
 
+    training_parameters = TrainingHyperparameters(
+        batch_size=128,
+        epochs=2,
+        learning_rate=0.002,
+        freeze_embeddings=False,
+        dropout=0.1
+    )
+    model_parameters = ModelHyperparameters(
+        comparison_embedding_size=128,
+        query_tower_hidden_dimensions=[128, 64],
+        doc_tower_hidden_dimensions=[128, 64],
+        include_layer_norms=True
+    )
+
+    doc_tower = DocumentTower(parameters=DocumentTowerParameters(
+        training=training_parameters,
+        model=model_parameters,
+        tokenizer=tokenizer,
+    ))
+    query_tower = QueryTower(parameters=QueryTowerParameters(
+        training=training_parameters,
+        model=model_parameters,
+        tokenizer=tokenizer,
+    ))
+
+    combined_parameters = list(doc_tower.parameters()) + list(query_tower.parameters())
     optimizer = optim.Adam(combined_parameters, lr=0.002)
 
     print_every = 100
@@ -157,7 +302,7 @@ if __name__ == "__main__":
     print("Beginning training...")
     for batch_idx, raw_batch in enumerate(train_data_loader):
         optimizer.zero_grad()
-        batch_results = process_test_batch(raw_batch, train_dataset, query_tower, doc_tower, device, margin)
+        batch_results = process_test_batch(tokenizer, raw_batch, train_dataset, query_tower, doc_tower, device, margin)
         loss = batch_results["total_loss"]
         running_samples += batch_results["document_count"]
         running_loss += loss.item()
